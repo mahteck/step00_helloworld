65ff9da5c8653d608361ef398416709b0e8f508f
mshoaib1244 <shoaib1244@gmail.com>
2024-03-04 18:56:56 +0500

    Third Commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 "use strict";
-let message = "Welcome";
+let message = "Welcome to Governer House";
+console.log(message);
+message = "This is 3rd Class Monday 7PM to 10PM 26th Feb 2024";
 console.log(message);
--- app.ts
+++ app.ts
@@ -1,2 +1,4 @@
 let message = "Hello World";
 console.log(message);
+let message1 = "This is 3rd Class 26th Feb 2024";
+console.log(message1);
